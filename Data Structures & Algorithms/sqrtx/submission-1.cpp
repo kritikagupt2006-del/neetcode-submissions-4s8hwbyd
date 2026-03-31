52d2ae49cba5514c5686705e1b3c1c147e8db080
kritikagupt2006-del <kritikagupt2006@gmail.com>
2026-03-31 19:31:28 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long low =1;
+        long long high = x;
+        while(low<=high){
+            long long mid = (low+high)/2;
+            if(mid*mid == x) return mid;
+            else if(mid* mid < x){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return high;
+    }
+};
